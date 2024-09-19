b833a181e48503550b6a173520c1e375b5cea190
denizis <121608927+denizis@users.noreply.github.com>
2024-09-19 15:48:02 +0300

    Proje dosyası ekle.

---
+++ MyDictionary/MyList.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    internal class MyList<T1,T2>
+    {
+    
+        T1[] keys;
+        T2[] values;
+
+
+        public MyList() {
+            
+            keys = new T1[0];
+             values = new T2[0];
+
+
+        }
+
+
+        public void Add(T1 key, T2 value)
+        {
+            T1[] tempKeys = keys;
+            T2[] tempValues = values;
+
+            keys = new T1[keys.Length + 1];
+            values = new T2[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+
+
+            }
+
+
+
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+
+
+            }
+       
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+
+
+
+
+        }
+
+
+
+
+        public T1[] GetKeys()
+        {
+            return keys;
+        }
+
+        // values dizisini dönen bir metot
+        public T2[] GetValues()
+        {
+            return values;
+        }
+
+        public int Count
+        {
+            get { return keys.Length; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            MyList<int, string> myDictionary = new MyList<int, string>();
+
+            
+            myDictionary.Add(1, "Deniz");
+            myDictionary.Add(2, "Cengiz");
+
+          
+            int[] keys = myDictionary.GetKeys();
+            string[] values = myDictionary.GetValues();
+
+            // Ekrana yazdırıyoruz
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("Key: {0} Value: {1}", keys[i], values[i]);
+            }
+
+            Console.Read();
+
+        }
+    }
+}
